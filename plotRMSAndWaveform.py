de247ce0cef725b67838252b6f9028915d92ed4a
Talel Zakhama <talelzakhama@Talels-MacBook-Pro-2.local>
2022-04-30 00:32:30 -0700

    Added argparse

--- plotRMSAndWaveform.py
+++ plotRMSAndWaveform.py
@@ -3,6 +3,15 @@ import numpy as np
 import librosa
 import librosa.display
 import IPython.display as ipd
+import argparse
+
+parser = argparse.ArgumentParser(description="Plotting RMS and waveform of a .wav file")
+parser.add_argument('--path',type=str,metavar='',required=True,help='Path to .wav file')
+parser.add_argument('--yRange',required=False,help='Range of Magnitude (dB) to show in the y axis')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q','--quiet',action='store_true',help='print quiet')
+group.add_argument('-v','--verbose',action='store_true',help='print verbose')
+args = parser.parse_args()
 
 def plotRMSAndWaveform(path,Frame_Length,Hop_Length):
 	file,sr=librosa.load (path)
@@ -11,18 +20,16 @@ def plotRMSAndWaveform(path,Frame_Length,Hop_Length):
 	frames=range(len(rms))
 	print (f"frames are {frames}\n")
 	t=librosa.frames_to_time(frames,hop_length=Hop_Length)
-
 	plt.figure(figsize=(15, 10))
 	librosa.display.waveplot(file, alpha=0.5)
 	plt.plot(t, rms, color="r")
 	plt.xlabel("Time (s)")
-	plt.ylim((-1, 1))
 	plt.ylabel("Magnitude (dB)")
 	plt.title("rms")
+	print ("Plot should now be open in a different window")
 	plt.show()
 
 
-path="/Users/talelzakhama/Downloads/POLQA-F2-S1-48k.wav"
 Frame_Length=1024
 Hop_Length=512
-plotRMSAndWaveform(path, Frame_Length, Hop_Length)
+plotRMSAndWaveform(args.path, Frame_Length, Hop_Length)
